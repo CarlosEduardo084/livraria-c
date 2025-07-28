93d6ea5e66ae459fcc71b7551556b1291b59751a
Carlos Eduardopng <carlsedu333@gmail.com>
2025-07-28 16:26:45 -0300

    primeiro commit

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <locale.h>
+#include "livraria.h"
+
+int main() {
+    setlocale(LC_ALL, "Portuguese");
+
+    if (login()) {
+        carregarLivrosDoArquivo();
+        printf("\nBem-vindo a Livraria Virtual!\n");
+        menu();
+    } else {
+        printf("Erro no login. Encerrando.\n");
+    }
+
+    return 0;
+}
